8d6d1025ae43dd29b6ccd147aaf0587c08270f0a
Simone Westphal <hello@swestphal.io>
2021-06-23 11:38:09 +0200

    refactor: Add team serializers and urls

--- apps/team/views.py
+++ apps/team/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TeamSerializer
+from .models import Team
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+
+class TeamViewSet(viewsets.ModelViewSet):
+    serializer_class = TeamSerializer
+    queryset = Team.objects.all()
+
+    def get_queryset(self):
+        teams = self.request.user.teams.all()
+
+        if not teams:
+            Team.object.create(name='', org_number='', created_by=self.request.user)
+
+        return self.queryset.filter(created_by=self.request.user)
+
+    def perform_create(self,serializer):
+        serializer.save(created_by=self.request.user)
+
+    def perform_update(self,serializer):
+        obj = self.get_object()
+
+        if self.request.user !=obj.created_by:
+            raise PermissionDenied('Wrong object owner')
+
+        serializer.save()
